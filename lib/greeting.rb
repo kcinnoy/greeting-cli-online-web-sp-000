f501bc75a85f2f143cf0e026d88af3f030820cbe
Yonnick Brown <25hour24@gmail.com>
2019-01-06 17:28:22 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,7 @@
 # code the #greeting method here!
 
+name = gets.chomp
+
 def greeting(name)
-  puts "Hello #{name.chomp}. It's nice to meet you."
+  puts "Hello #{name}. It's nice to meet you."
 end
